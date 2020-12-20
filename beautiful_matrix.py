dc2c0fc0ab063956ca0af269bd35bf62364ac18e
Israel Teneda <israteneda@gmail.com>
2020-12-19 21:53:07 -0500

    feat: Beautiful Matrix exercise

---
+++ beautiful_matrix.py
@@ -0,0 +1,63 @@
+""" 
+A. Beautiful Matrix
+https://codeforces.com/problemset/problem/263/A
+"""
+
+import io
+from unittest import mock
+from unittest.mock import patch
+
+def solve():
+    matrix = []
+    moves = 0
+
+    for _ in range(5):
+        row = input().split()
+        matrix.append(row)
+
+    while matrix[2][2] != '1':
+        for r, row in enumerate(matrix): 
+            for c, column in enumerate(row):
+                if column == '1':
+                    if c < 2:
+                        matrix[r][c], matrix[r][c + 1] = matrix[r][c + 1], matrix[r][c]
+                        moves += 1
+                    elif c > 2:
+                        matrix[r][c], matrix[r][c - 1] = matrix[r][c - 1], matrix[r][c]
+                        moves += 1
+
+            if '1' in row:
+                if r < 2:
+                    matrix[r], matrix[r + 1] = matrix[r + 1], matrix[r]
+                    moves += 1
+                elif r > 2:
+                    matrix[r], matrix[r - 1] = matrix[r - 1], matrix[r]
+                    moves += 1
+
+    print(moves) 
+
+if __name__ == '__main__':
+    solve()
+
+@patch('sys.stdout', new_callable=io.StringIO)
+def test(mock_stdout):
+    user_input = [
+            '0 0 0 0 0',
+            '0 0 0 0 1',
+            '0 0 0 0 0',
+            '0 0 0 0 0',
+            '0 0 0 0 0',
+            '0 0 0 0 0',
+            '0 0 0 0 0',
+            '0 1 0 0 0',
+            '0 0 0 0 0',
+            '0 0 0 0 0',
+            ]
+
+    with patch('builtins.input', side_effect=user_input):
+        for _ in range(2):
+            solve()
+
+    assert mock_stdout.getvalue() == '3\n1\n'
+
+
--- george_and_acommodation.py
+++ george_and_acommodation.py
@@ -1,15 +1,6 @@
 """
-George has recently entered the BSUCP (Berland State University for Cool Programmers). George has a friend Alex who has also entered the university. Now they are moving into a dormitory.
-
-George and Alex want to live in the same room. The dormitory has n rooms in total. At the moment the i-th room has pi people living in it and the room can accommodate qi people in total (pi ≤ qi). Your task is to count how many rooms has free place for both George and Alex.
-
-Input
-The first line contains a single integer n (1 ≤ n ≤ 100) — the number of rooms.
-
-The i-th of the next n lines contains two integers pi and qi (0 ≤ pi ≤ qi ≤ 100) — the number of people who already live in the i-th room and the room's capacity.
-
-Output
-Print a single integer — the number of rooms where George and Alex can move in.
+A. George and Accommodation
+https://codeforces.com/problemset/problem/467/A
 """
 
 import io
@@ -21,9 +12,8 @@ def solve():
     available_rooms = 0
     for _ in range(rooms):
         p, q = [ int(num) for num in input().split(' ')]
-        if p < q:
-            if p + 2 <= q:
-                available_rooms += 1
+        if p + 2 <= q:
+            available_rooms += 1
     print(available_rooms)
 
 @patch('sys.stdout', new_callable=io.StringIO)
